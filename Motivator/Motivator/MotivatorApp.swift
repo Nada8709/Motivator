6cb58fae0de9cbd94cdc23776e2d1b3bb2ed8267
Nada Ashraf <Nada.Ashraf@mdi.com.eg>
2023-09-12 19:17:49 +0300

    no message

---
+++ Motivator/Motivator/MotivatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotivatorApp.swift
+//  Motivator
+//
+//  Created by Nada Ashraf on 12/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct MotivatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
